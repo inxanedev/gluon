2fbe392cde8267e6e9cb6a47f05836b25a0f9c56
Remigiusz Schoida <inxaneninja@gmail.com>
2023-05-11 14:17:50 +0200

    Updated configs to prefer server configuration in most cases

--- src/main/java/com/inxane/gluon/config/ModConfigModel.java
+++ src/main/java/com/inxane/gluon/config/ModConfigModel.java
@@ -1,21 +1,29 @@
 package com.inxane.gluon.config;
 
+import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.config.annotation.Nest;
+import io.wispforest.owo.config.annotation.Sync;
 
 @Modmenu(modId = "gluon")
 @Config(name = "gluon", wrapperName = "ModConfig")
 @SuppressWarnings("unused")
 public class ModConfigModel {
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public boolean leadableVillagers = true;
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public boolean unbreakableLeads = true;
 
     @Nest
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public PiglinBrutesDropNetheriteScrap piglinBrutesDropNetheriteScrap = new PiglinBrutesDropNetheriteScrap();
     public static class PiglinBrutesDropNetheriteScrap {
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
         public boolean enabled = true;
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
         public float chance = 55.0f;
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
         public float additionalChancePerLootingLevel = 15.0f;
     }
 
@@ -26,6 +34,8 @@ public class ModConfigModel {
         public boolean throughItemFrames = true;
     }
 
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public int afterBarterPiglinAngerCooldown = 20 * 15;
+    @Sync(Option.SyncMode.OVERRIDE_CLIENT)
     public boolean spawnParticlesOnPiglinAnger = true;
 }
